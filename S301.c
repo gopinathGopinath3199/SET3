c8b4cf34badf87e7a950440d029faab8fab66bdf
gopinathGopinath3199 <47105587+gopinathGopinath3199@users.noreply.github.com>
2019-03-13 16:33:47 +0530

    Create S301.c

---
+++ S301.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,n,d,i,j;
+int N,A,D;
+printf("enter the value of a");
+scanf("%d",&a);
+printf("enter the value of n");
+scanf("%d",&n);
+printf("enter the value of d");
+scanf("%d",&d);
+N=n-1;
+D=N*d;
+A=2*a;
+s=A+D;
+sum=s*n/2;
+printf("%d",sum);
+getch();
+}
