0a23a22fb44f96ec80d7af8d144af6b96c0bd329
gopinathGopinath3199 <47105587+gopinathGopinath3199@users.noreply.github.com>
2019-04-01 15:32:24 +0530

    Update S301.c

--- S301.c
+++ S301.c
@@ -5,9 +5,9 @@ void main()
 int a,n,d,i,j,sum,s;
 int N,A,D;
 printf("enter the value of n");
-scanf("%d",&a);
-printf("enter the value of a");
 scanf("%d",&n);
+printf("enter the value of a");
+scanf("%d",&a);
 printf("enter the value of d");
 scanf("%d",&d);
 N=n-1;
